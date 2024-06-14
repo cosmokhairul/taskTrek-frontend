31f4790f586253e13d92203890e2f77e7069f3cb
Khairul <porosh.means.touch@gmail.com>
2024-06-14 18:11:21 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../components/home/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+            <Banner />
+        </div>
+    );
+};
+
+export default Home;
